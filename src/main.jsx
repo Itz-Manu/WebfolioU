9b7270e485c1fa88df1351695f03842a8cd1b286
Manendra Verma <manuvma03@gmail.com>
2024-02-14 14:49:52 +0530

    Fix: Router Error

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
-import Router from "./routes/routes";
-
+import Router from './routes/Routes'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
